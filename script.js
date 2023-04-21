bbb720cf6873e3bc2e7d8187277b7410ad84c84c
tienbot1 <tien.ru@mail.ru>
2023-04-21 20:27:06 +0900

    get news by click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//Получаем категории новостей из БД
 function categoryNews(){
-    for(i=11; i<15; i++){
+    for(i=103; i<=106; i++){
         fetch(`http://24api.ru/rest-news-category/${i}`, {
         "method": "GET"
         })
@@ -9,22 +10,22 @@ function categoryNews(){
 }
 categoryNews()
 
-function news(){
-    for(i=3; i<8; i++){
-        fetch(`http://24api.ru/rest-news/${i}`, {
-        "method": "GET"
-        })
-        .then(data => data.json())
-        .then(data => newsAtSite(data))
-    }
-}
-news()
-
+//Отрисовываем категории
 function categoryAtSite(obj) {
     const container = document.body.querySelector(".category__wrapper")
-    
     const category = document.createElement('div')
-    category.classList.add(`category`)
+    category.classList.add('category')
+    //Слушатель категорий
+    category.addEventListener('click', () =>{
+        let allCategory = document.querySelectorAll('.category')
+        allCategory.forEach(el => {
+            el.classList.remove('active')
+        })
+
+        container.parentNode.children[1].innerHTML=''
+        category.classList.add('active')
+        news(obj.id) //передаем id категории в функцию news
+    })
 
     const p = document.createElement('p')
     p.innerText = obj.name
@@ -33,30 +34,36 @@ function categoryAtSite(obj) {
     category.append(p)
 }
 
-function newsAtSite(obj){
-    const container = document.body.querySelector(".news__wrapper")
-
-    const news = document.createElement('div')
-    news.classList.add(`news`)
-
-    const title = document.createElement('p')
-    title.classList.add('title')
-    title.innerText = obj.title
-    const text = document.createElement('p')
-    title.classList.add('text')
-    text.innerText = obj.body
-
-    container.append(news)
-    news.append(title, text)
+//Получаем новости из БД
+function news(id){ //принимаем id категории...
+    for(i=11; i<=20; i++){
+        fetch(`http://24api.ru/rest-news/${i}`, {
+        "method": "GET"
+        })
+        .then(data => data.json())
+        .then(data => newsAtSite(data, id)) //... и передаем его в функцию newsAtSite вместе с данными
+    }
 }
 
-// let categoryA = document.querySelectorAll('.category')
+//Отрисовываем новости на сайте
+function newsAtSite(obj, numb){ //получаем данные и id категории (numb)...
+    if(obj.category_id == numb){ // если категория новости та же, что мы передаем - отображаем ее на сайте
+        const container = document.body.querySelector(".news__wrapper")
 
-// function click (element){
-//     element.forEach( el => {
-//         // el.classList.remove('active')
-//         el.addEventListener('click', () =>{
-//             el.classList.toggle('active')
-//         })
-//     })
-// }
+        const news = document.createElement('div')
+        news.classList.add(`news`)
+
+        const title = document.createElement('p')
+        title.classList.add('text')
+
+        const text = document.createElement('p')
+        text.innerText = obj.body
+
+        container.append(news)
+        news.append(title, text)
+    }
+}
+
+function notActive(obj){
+    obj.classList.remove('active')
+}
